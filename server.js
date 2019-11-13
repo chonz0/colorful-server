919090cf3e147ab0aa561629b21cc5c4d38f8768
Gonzalo Quinteros <chonz0@gmail.com>
2019-11-13 12:25:36 -0300

    Change port

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ wss.on('connection', (ws) => {
 
 // start our server
 try {
-  // const port = process.env.PORT || 8999;
-  const port = 3000;
+  const port = process.env.PORT || 8999;
+  // const port = 3000;
   server.listen(port, () => {
     console.log(`Server started on port ${server.address().port} :)`);
   });
